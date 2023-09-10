38a8b77cf74035e668126f8bf8d028f4803c0b4e
RomitMaverick <romit.dgupta@gmail.com>
2023-09-10 13:05:24 +0530

    episode-01

---
+++ index.js
@@ -0,0 +1,13 @@
+const parent = React.createElement('div',
+    {id: 'parent'},
+    [React.createElement('div', {id: 'child'},
+        [React.createElement('h1',{className: 'heading'},'Hello World from React h1!!!'),
+        React.createElement('h2',{className: 'heading2'},'Hello World from React h2!!!')]
+    ),
+    React.createElement('div', {id: 'child'},
+        [React.createElement('h1',{className: 'heading'},'Hello World from React h1!!!'),
+        React.createElement('h2',{className: 'heading2'},'Hello World from React h2!!!')]
+    )]
+);
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(parent);
